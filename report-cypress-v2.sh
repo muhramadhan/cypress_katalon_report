7921f8af0acad879ddfb4fc00612665192cab893
daecide <ferry.sutanto@tokopedia.com>
2019-11-22 03:22:10 +0000

    add scripts for bvt json version

---
+++ report-cypress-v2.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+usage(){
+    echo "Usage: [-r | --reportDir ReportPath] [-c | --config configFile] [-s | --service service_name]"
+}
+
+FOLDER_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+REPORT_MOCHAWESOME_SERVICE_PATH=
+TEST_CONFIG_FILEPATH=
+SERVICE_NAME=
+
+while getopts "r:c:s:-:" options; do
+    case ${options} in
+        -)
+            case "${OPTARG}" in
+                reportDir)
+                    REPORT_MOCHAWESOME_SERVICE_PATH=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+                config)
+                    TEST_CONFIG_FILEPATH=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+                service)
+                    SERVICE_NAME=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+            esac;;
+        r)
+            REPORT_MOCHAWESOME_SERVICE_PATH=${OPTARG}
+            ;;
+        c)
+            TEST_CONFIG_FILEPATH=${OPTARG}
+            ;;
+        s)
+            SERVICE_NAME=${OPTARG}
+            ;;
+        :)
+            echo "option -${OPTARG} requires an argument"
+            usage
+            exit 1
+            ;;
+        *)
+            echo ${options}
+            usage
+            exit 1
+            ;;
+
+    esac
+done
+
+if [ -z "$REPORT_MOCHAWESOME_SERVICE_PATH" ] || [ -z "$TEST_CONFIG_FILEPATH" ]
+then
+    echo "-r, -c and -s arguments needed"
+    usage
+    exit 1
+fi
+
+if [ ! -f $REPORT_MOCHAWESOME_SERVICE_PATH/mochawesome.json ]
+then
+    echo "Error: There is no report file in $REPORT_MOCHAWESOME_SERVICE_PATH"
+    exit 1
+fi
+
+MERGED=false
+# echo "Merging mochawesome jsons"
+mkdir -p $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged
+npx mochawesome-merge --reportDir $REPORT_MOCHAWESOME_SERVICE_PATH > $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged/mochawesome-merge.json
+if [ $? -eq 0 ]
+then
+    MERGED=true
+else
+    echo "failed to merge"
+    exit 1
+fi
+
+#GENERATE HTML REPORT
+# if [ $MERGED = true ]
+# then
+#     echo "Generating html report"
+#     if [ ! -f "$REPORT_MOCHAWESOME_SERVICE_PATH/merge/mochawesome-merge.json" ]
+#     then
+#         echo "Error: mochawesome-merge.json not found"
+#         exit 1
+#     fi
+#     npx mochawesome-report-generator $REPORT_MOCHAWESOME_SERVICE_PATH/merge/mochawesome-merge.json -o $REPORT_MOCHAWESOME_SERVICE_PATH/html/
+# fi
+
+
+# echo "Extracting test results information"
+
+test_name=$(jq -r '.env.test_name' cypress/config/$TEST_CONFIG_FILEPATH.json)
+test_env=$(jq -r '.env.env_name' cypress/config/$TEST_CONFIG_FILEPATH.json)
+num_tests=$(jq -r '.stats.tests' $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged/mochawesome-merge.json)
+num_suites=$(jq -r '.stats.suites' $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged/mochawesome-merge.json)
+failures=$(jq -r '.stats.failures' $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged/mochawesome-merge.json)
+payload=
+
+service_lower=$(echo "$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
+
+if [ -n "$test_name" ] && [ -n "$test_env" ]
+then
+    payload='
+    {
+        "text":"*Cypress Staging Automation Report*\n\n*Service Name: '$SERVICE_NAME'*\n*Test Name: '$test_name'*\n*Env: '$test_env'*\n*Number of test suite(s): '$num_suites'*\t*Number of test(s): '$num_tests'*\n*Failures: '$failures'*\n",
+        "attachments": [
+            {
+                "color": "#458b00",
+                "fallback": "Jenkins url: '${BUILD_URL}'",
+                "actions":[
+                    {
+                        "type": "button",
+                        "text": "Jenkins",
+                        "url": "'${BUILD_URL}'",
+                        "style":"primary"
+                    },{
+                        "type": "button",
+                        "text": "HTML Report",
+                        "url": "",
+                        "style":"primary"
+                    }
+                ]
+            }
+        ]
+    }
+    '
+else
+    payload='
+    {
+        "text":"*Cypress Staging Automation Report*\n\n*Service Name: '$SERVICE_NAME'*\n*Number of test suite(s): '$num_suites'*\t*Number of test(s): '$num_tests'*\n*Failures: '$failures'*\n",
+        "attachments": [
+            {
+                "color": "#458b00",
+                "fallback": "Jenkins url: '${BUILD_URL}'",
+                "actions":[
+                    {
+                        "type": "button",
+                        "text": "Jenkins",
+                        "url": "'${BUILD_URL}'",
+                        "style":"primary"
+                    },{
+                        "type": "button",
+                        "text": "HTML Report",
+                        "url": "",
+                        "style":"primary"
+                    }
+                ]
+            }
+        ]
+    }
+    '
+fi
+
+indexSuite=0
+slack=false
+for i in $(jq -r ".suites.suites[] | @base64" $REPORT_MOCHAWESOME_SERVICE_PATH/../${FOLDER_TIMESTAMP}_merged/mochawesome-merge.json)
+do 
+    # echo $i | base64 --decode | jq -r ".title"
+    footerElem=
+    pass=true
+
+    for j in $(echo $i | base64 --decode | jq -r 'map(.. | select(.fail? == true)) | .[] | @base64')
+    do    
+        decodedTestCase=$(echo $j | base64 --decode)
+        # echo $decodedTestCase | tr '\r\n' ' ' | jq -r ".title"
+        if [ $(echo $decodedTestCase | tr '\r\n' ' ' | jq -r ".fail") = true ]
+        then
+            pass=false
+            slack=true
+            testCaseTitle=$(echo $decodedTestCase | tr '\r\n' ' ' | jq -r ".title")
+            errMsg=$(echo $decodedTestCase | tr '\r\n' ' ' | jq -r ".err.message" | tr '\n' ' ' | cut -c1-55)
+            footerElem+='-'$testCaseTitle': '$(echo $errMsg)'\n'
+        fi
+    done
+    if [ -z "$footerElem" ]
+    then
+        footerElem=""
+    fi
+    if [ "$pass" = false ]
+    then
+        suiteTitle=$(echo $i | base64 --decode | jq -r ".title" | sed 's/\"/\\"/g' )
+	#suiteTitle=${suiteTitle//\"/\\"}
+        attachmentElem='
+        {
+            "color": "#8b0000",
+            "title": "Test Suite '$((indexSuite+1))': \"'$suiteTitle'\"",
+            "footer": "'$(echo $footerElem | sed 's/\\'"'"'/'"\""'/g' | sed 's/\"/\\"/g' | sed 's/'"'"'/\\"/g')'"
+        }
+        '
+        payload=$(jq ".attachments += [$attachmentElem]" <<< "$payload")
+    fi
+    ((indexSuite++))
+done
+
+
+#if [ $slack = true ]
+#then
+    echo $payload
+    # curl -i -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_HOOK"
+#fi
+
+#https://hooks.slack.com/services/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
+#Cleaning report files
+# rm $REPORT_MOCHAWESOME_SERVICE_PATH/*.json
+# rm $REPORT_MOCHAWESOME_SERVICE_PATH/../merge/*.json
---
+++ report-katalon-v2.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+
+usage(){
+    echo "Usage: [-r | --reportDir ReportPath] [-t | --timestamps \"time1,time2,time3\"]] [-s | --service service_name]"
+}
+
+
+REPORT_DIR=
+SERVICE_NAME=
+TIMESTAMPS=
+
+# Input param
+while getopts "r:t:s:-:" options; do
+    case ${options} in
+        -)
+            case "${OPTARG}" in
+                reportDir)
+                    REPORT_MOCHAWESOME_SERVICE_PATH=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+                service)
+                    SERVICE_NAME=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+                timestamps)
+                    TIMESTAMPS=${!OPTIND}; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+            esac;;
+        r)
+            REPORT_DIR=${OPTARG}
+            ;;
+        s)
+            SERVICE_NAME=${OPTARG}
+            ;;
+        t)
+            TIMESTAMPS=${OPTARG}
+            ;;
+        :)
+            echo "option -${OPTARG} requires an argument"
+            usage
+            exit 1
+            ;;
+        *)
+            echo ${options}
+            usage
+            exit 1
+            ;;
+
+    esac
+done
+
+# payload='
+# {
+#     "text":"*Katalon Staging Automation Report*\n\n*Service Name: '$SERVICE_NAME'*\n*Test Name: '$TEST_NAME'*\n*Number of test(s): '$num_tests'*\t*Passed: '$passed'*\t*Failures: '$failures'*\t*Errors: '$errors'*\n",
+#     "attachments": [
+#         {
+#             "color": "#458b00",
+#             "fallback": "Jenkins url: '$BUILD_URL'",
+#             "actions":[
+#                 {
+#                     "type": "button",
+#                     "text": "Jenkins",
+#                     "url": "'$BUILD_URL'",
+#                     "style":"primary"
+#                 },{
+#                     "type": "button",
+#                     "text": "HTML Report",
+#                     "url": "",
+#                     "style":"primary"
+#                 }
+#             ]
+#         }
+#     ]
+# }
+# '
+payload='
+{
+    "attachments": [
+        {
+            "color": "#458b00",
+            "fallback": "Jenkins url: '$BUILD_URL'",
+            "actions":[
+                {
+                    "type": "button",
+                    "text": "Jenkins",
+                    "url": "'$BUILD_URL'",
+                    "style":"primary"
+                },{
+                    "type": "button",
+                    "text": "HTML Report",
+                    "url": "",
+                    "style":"primary"
+                }
+            ]
+        }
+    ]
+}
+'
+
+time_arr=(${TIMESTAMPS//,/ })
+passed_total=0
+failures_total=0
+errors_total=0
+num_tests_total=0
+SLACK=false
+for curr_timestamp in ${time_arr[@]}
+do
+    # Extracting test information
+    ## index start from 1
+    # test_name=$(xmllint --xpath "string(//testsuite[1]/@id)" $REPORT_DIR/JUnit_Report.xml) #asumsi test suite cuma 1
+    passed=$(xmllint --xpath "string(//testsuite[1]/@tests)" $REPORT_DIR/${curr_timestamp}/JUnit_Report.xml)
+    failures=$(xmllint --xpath "string(//testsuite[1]/@failures)" $REPORT_DIR/${curr_timestamp}/JUnit_Report.xml)
+    errors=$(xmllint --xpath "string(//testsuite[1]/@errors)" $REPORT_DIR/${curr_timestamp}/JUnit_Report.xml)
+    num_tests=$((passed + failures + errors))
+
+    passed_total=$((passed_total + passed))
+    failures_total=$((failures_total + failures))
+    errors_total=$((errors_total + errors))
+    num_tests_total=$((num_tests_total + num_tests))
+    
+
+    ##Loop failed testcase(s)
+    INDEX=1
+    while [[ $INDEX -le $failures ]];do
+            testcaseName=$(xmllint --xpath "string(//testcase[@status='FAILED'][$INDEX]/@name)" $REPORT_DIR/${curr_timestamp}/JUnit_Report.xml)
+
+            attachmentElem='
+            {
+                "color": "#8b0000",
+                "title": "\"'$testcaseName'\""
+            }
+            '
+            payload=$(jq ".attachments += [$attachmentElem]" <<< "$payload")
+            SLACK=true
+            ((INDEX++))
+    done
+
+    INDEX=1
+    while [[ $INDEX -le $errors ]];do
+            testcaseName=$(xmllint --xpath "string(//testcase[@status='ERROR'][$INDEX]/@name)" $REPORT_DIR/${curr_timestamp}/JUnit_Report.xml)
+
+            attachmentElem='
+            {
+                "color": "#d07e00",
+                "title": "\"'$testcaseName'\""
+            }
+            '
+            payload=$(jq ".attachments += [$attachmentElem]" <<< "$payload")
+            SLACK=true
+            ((INDEX++))
+    done
+done
+
+headerText='"*Katalon Staging Automation Report*\n\n*Service Name: '$SERVICE_NAME'*\n*Test Name: '$TEST_NAME'*\n*Number of test(s): '$num_tests_total'*\t*Passed: '$passed_total'*\t*Failures: '$failures_total'*\t*Errors: '$errors_total'*\n"'
+
+payload=$(jq ". += {\"text\": $(echo $headerText)}" <<< "$payload")
+if [[ $SLACK = true ]]
+then
+    echo $payload
+    # curl -i -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_HOOK"
+fi
